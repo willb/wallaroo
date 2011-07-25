85177d0ed831ae4665ca2fb2d44506ae38bcd946
Will Benton <willb@redhat.com>
2011-07-25 16:49:49 -0500

    refactored tagging to use wallaby_mixin.py; added wallaby_collections mixin

--- python-wallaroo/tagging.py
+++ python-wallaroo/tagging.py
@@ -15,52 +15,10 @@
 # limitations under the License.
 
 import wallaby
+from wallaby_mixin import patch_to, calculated_attribute
 
 PARTITION_GROUP = "===TAGS_BELOW==="
 store = None
-
-def patch_to(klass):
-    def do_mixin(target):
-        setattr(klass, target.__name__, target)
-        return target
-    return do_mixin
-
-def calculated_attribute(klass, attr_name):
-    """
-# Think of this as a way to add an attribute/property to another class.
-# Example usage:
-class Foo(object):
-    pass
-
-class Blah(object):
-    # NB:  you cannot call this with a reference to the class you're declaring
-    @calculated_attribute(Foo, "bar")
-    def myBlah(self):
-        return self.blah
-    def setBlah(self, b):
-        self.blah = b
-    """
-    def do_calculated_attribute(target):
-        def default_fallback(self, name):
-            raise AttributeError("%r has no attribute named %r") % (self,name)
-        fallback = None
-        
-        if "__getattr__" in dir(klass):
-            fallback = klass.__getattr__
-        else:
-            fallback = default_fallback
-        
-        def calc_attr(self, name):
-            if name == attr_name:
-                return target(self)
-            else:
-                return fallback(self, name)
-        
-        klass.__getattr__ = calc_attr
-        
-        return target
-    
-    return do_calculated_attribute
 
 def uniq(ls):
   def gen_check_and_add(d):
