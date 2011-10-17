d0006a9f560d0f14b62038f0f91214d356414acb
Will Benton <willb@redhat.com>
2011-10-17 00:00:12 -0500

    using SHA-1 instead of MD5

--- wallaroo/src/wallaroo_hash.erl
+++ wallaroo/src/wallaroo_hash.erl
@@ -5,21 +5,23 @@
 -export([as_bitstring/1, as_string/1, as_num/1, hash_to_bitstring/1, hash_to_num/1]).
 
 as_bitstring(Object) ->
-    crypto:md5(term_to_binary(Object)).
+    crypto:sha(term_to_binary(Object)).
 
 as_string(Object) ->
-    lists:flatten(io_lib:format("~32.16.0b", [as_num(Object)])).
+    lists:flatten(io_lib:format("~40.16.0b", [as_num(Object)])).
 
 as_num(Object) ->
-    <<SHA:128/big-unsigned-integer>> = as_bitstring(Object),
+    generic_to_num(Object, fun wallaroo_hash:as_bitstring/1).
+
+generic_to_num(Object, F) ->
+    <<SHA:160/big-unsigned-integer>> = F(Object),
     SHA.
 
 hash_to_bitstring(S) ->
-    << <<(wallaroo_db:hexdigit_to_int(C)):4>> || C <- S >>.
+    << <<(hexdigit_to_int(C)):4>> || C <- S >>.
 
 hash_to_num(S) ->
-    <<SHA:128/big-unsigned-integer>> = hash_to_bitstring(S),
-    SHA.
+    generic_to_num(S, fun wallaroo_hash:hash_to_bitstring/1).
 
 hexdigit_to_int(I) when I =< $9 andalso I >= $0 ->
     I - $0;
