462d9a54441133b86520d2dc01ca5c75ae86ea94
Will Benton <willbenton@gmail.com>
2011-12-07 17:22:27 -0600

    Updated wallaroo_tree to use storage modules.

--- src/wallaroo_db.erl
+++ src/wallaroo_db.erl
@@ -1,8 +1,16 @@
-% Low-level DB functionality for Wallaroo
+% High-level DB functionality for Wallaroo
 % Copyright (c) 2011 Red Hat, Inc., and William C. Benton
 
 -module(wallaroo_db).
 -compile(export_all).
+
+hash_and_store(Object, StoreMod) ->
+    hash_and_store(Object, StoreMod, store_object).
+
+hash_and_store(Object, StoreMod, Func) ->
+    SHA = wallaroo_hash:as_bitstring(Object),
+    StoreMod:Func(SHA, Object),
+    {SHA, Object}.
 
 store(Object) ->
     ok.
