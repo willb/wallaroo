f417778d6dfe2c2aa6d872fc6a37534917e445ec
Will Benton <willb@redhat.com>
2011-12-15 02:12:37 -0600

    added simple wallaby group/node representations

---
+++ wallaroo/src/wallaby_node.erl
@@ -0,0 +1,27 @@
+% Classic Wallaby node functionality
+% Copyright (c) 2011 Red Hat, Inc. and William C. Benton
+
+-module(wallaby_node).
+-compile(export_all).
+
+new(Name, Provisioned) when is_binary(Name) ->
+    Dict = orddict:from_list([{name, Name}, {memberships, []}, {identity_group, nil}, {provisioned, Provisioned}, {last_checkin, 0}, {last_updated_version, 0}]),
+    {wallaby_node, Dict}.
+
+name({wallaby_node, Dict}) -> orddict:fetch(name, Dict).
+
+provisioned({wallaby_node, Dict}) -> orddict:fetch(provisioned, Dict).
+
+last_checkin({wallaby_node, Dict}) -> orddict:fetch(last_checkin, Dict).
+
+last_updated_version({wallaby_node, Dict}) -> orddict:fetch(last_updated_version, Dict).
+
+identity_group({wallaby_node, Dict}) -> orddict:fetch(identity_group, Dict).
+
+memberships({wallaby_node, Dict}) -> orddict:fetch(memberships, Dict).
+
+set_memberships({wallaby_node, Dict}, Memberships) ->
+    {wallaby_node, orddict:store(memberships, Memberships, Dict)}.
+
+make_provisioned({wallaby_node, Dict}) ->
+    {wallaby_node, orddict:store(provisioned, true, Dict)}.
