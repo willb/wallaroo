534d7a862191b296f6265647ea3c4b62f4dac714
Will Benton <willb@redhat.com>
2011-12-15 01:14:31 -0600

    many fixes to wallaroo API

--- wallaroo/src/wallaroo.erl
+++ wallaroo/src/wallaroo.erl
@@ -13,8 +13,15 @@
 -define(SERVER, ?MODULE).
 
 start_link() ->
-    wallaroo_store_ets:init(),
-    gen_server:start_link(?MODULE, [], []).
+    error_logger:info_msg("entering WALLAROO start_link/0 ~n", []),
+    wallaroo_store_ets:init([]),
+    Result = try 
+		 gen_server:start_link({local, ?SERVER}, ?MODULE, [], [])
+	     catch
+		 Kind:Reason -> error_logger:info_msg("failure! ~p:~p~n", [Kind, Reason]), {failure_with, {Kind, Reason}}
+	     end,
+    error_logger:info_msg("returning ~p from WALLAROO start_link/0 ~n", [Result]),
+    Result.
 
 init([]) ->
     {ok, {wallaroo_store_ets}}.
@@ -38,7 +45,7 @@ get_node(Name) ->
     end.
 
 get_node(Name, Commit) ->
-    wallaroo ! {get, node, Name, canonicalize_hash(Commit)}.
+    gen_server:call(wallaroo,  {get, node, Name, canonicalize_hash(Commit)}).
 
 get_group(Name) ->
     case get_tag("current") of
@@ -50,28 +57,28 @@ get_group(Name) ->
     end.
 
 get_group(Name, Commit) ->
-    wallaroo ! {get, group, Name, canonicalize_hash(Commit)}.
+    gen_server:call(wallaroo,  {get, group, Name, canonicalize_hash(Commit)}).
 
 get_tag(Name) ->
-    wallaroo ! {get_tag, Name}.
+    gen_server:call(wallaroo,  {get_tag, Name}).
 
 put_tag(Name, C) ->
     Commit = canonicalize_hash(C),
-    wallaroo ! {put_tag, Name, Commit}.
+    gen_server:call(wallaroo,  {put_tag, Name, Commit}).
 
 put_group(Name, {wallaby_group, [_|_]}=Group) ->
-    wallaroo ! {put, group, Name, Group}.
+    gen_server:call(wallaroo,  {put, group, Name, Group}).
 
 put_node(Name, {wallaby_node, [_|_]}=Node) ->
-    wallaroo ! {put, node, Name, Node}.
+    gen_server:call(wallaroo,  {put, node, Name, Node}).
 
 put_group(Name, {wallaby_group, [_|_]}=Group, SC) ->
     C = canonicalize_hash(SC),
-    wallaroo ! {put, group, Name, Group, C}.
+    gen_server:call(wallaroo,  {put, group, Name, Group, C}).
 
 put_node(Name, {wallaby_node, [_|_]}=Node, SC) ->
     C = canonicalize_hash(SC),
-    wallaroo ! {put, node, Name, Node, C}.
+    gen_server:call(wallaroo,  {put, node, Name, Node, C}).
 
 
 %%% gen_server callbacks
@@ -81,15 +88,15 @@ handle_cast(stop, State) ->
 
 handle_call({get, What, Name, StartingCommit}, _From, {StoreMod}=State) when What=:=group; What=:=node ->
     CommitObj = get_commit(StartingCommit, StoreMod),
-    Tree = wallaroo_commit:get_tree(CommitObj),
+    Tree = wallaroo_commit:get_tree(CommitObj, StoreMod),
     {reply, get_path(What, Name, Tree, StoreMod), State};
 handle_call({put, What, Name, Value, StartingCommit}, _From, {StoreMod}=State) when What=:=group; What=:=node ->
     CommitObj = get_commit(StartingCommit, StoreMod),
-    Tree = wallaroo_commit:get_tree(CommitObj),
-    {reply, put_path(What, Name, Value, Tree, StoreMod), State};
+    Tree = wallaroo_commit:get_tree(CommitObj, StoreMod),
+    {reply, put_path(What, Name, Value, Tree, StoreMod, StartingCommit), State};
 handle_call({put, What, Name, Value}, _From, {StoreMod}=State) when What=:=group; What=:=node ->
     Tree = wallaroo_tree:empty(),
-    {reply, put_path(What, Name, Value, Tree, StoreMod), State};
+    {reply, put_path(What, Name, Value, Tree, StoreMod, empty), State};
 handle_call({get_tag, Name}, _From, {StoreMod}=State) ->
     TagObj = StoreMod:find_tag(Name),
     {reply, TagObj, State};
@@ -115,13 +122,20 @@ canonicalize_hash(BS) when is_binary(BS) ->
 
 
 get_path(What, Name, Tree, StoreMod) ->
-    Path = [What ++ "s", Name],
+    Path = [atom_to_list(What) ++ "s", Name],
     wallaroo_tree:get_path(Path, Tree, StoreMod).
 
-put_path(What, Name, Value, Tree, StoreMod) ->
-    Path = [What ++ "s", Name],
-    wallaroo_tree:put_path(Path, Value, Tree, StoreMod).
+put_path(What, Name, Value, Tree, StoreMod, empty) ->
+    Commit = wallaroo_commit:store(wallaroo_commit:empty(), StoreMod),
+    put_path(What, Name, Value, Tree, StoreMod, Commit);
+put_path(What, Name, Value, Tree, StoreMod, ParentCommit) ->
+    Path = [atom_to_list(What) ++ "s", Name],
+    {NewTree, _} = wallaroo_tree:put_path(Path, Value, Tree, StoreMod),
+    Commit = wallaroo_commit:new([ParentCommit], NewTree, [], []),
+    wallaroo_commit:store(Commit, StoreMod).
 
 get_commit(SHA, StoreMod) ->
-    StoreMod:find_commit(SHA).
+    Result = StoreMod:find_commit(SHA),
+    error_logger:info_msg("wallaroo:get_commit/2 -> ~p ~n", [Result]),
+    Result.
 
