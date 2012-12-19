12359afc0035e9ef4d9f22c1aed2366b265702b2
William Benton <willb@redhat.com>
2012-12-19 12:19:56 -0600

    tag and branch resources refactored

--- wallaroo_web/src/wallaroo_branch_resource.erl
+++ wallaroo_web/src/wallaroo_branch_resource.erl
@@ -1,8 +1,8 @@
 %% @author William C. Benton <willb@redhat.com>
-%% @copyright 2011 Red Hat, Inc. and William C. Benton
-%% @doc Wallaroo tag resource
+%% @copyright 2012 Red Hat, Inc. and William C. Benton
+%% @doc Wallaroo branch resource
 
--module(wallaroo_tag_resource).
+-module(wallaroo_branch_resource).
 -export([init/1, to_json/2, resource_exists/2]). 
 -export([from_json/2]).
 -export([allowed_methods/2, content_types_provided/2, content_types_accepted/2, finish_request/2]).
@@ -16,7 +16,7 @@ allowed_methods(ReqData, Ctx) ->
     {['HEAD', 'GET', 'POST', 'PUT'], ReqData, Ctx}.
 
 resource_exists(ReqData, Ctx) ->
-    wallaroo_web_common:generic_entity_exists_nc(ReqData, Ctx, fun(Name) -> wallaroo:get_tag(Name) end, tag).
+    wallaroo_web_common:generic_entity_exists_nc(ReqData, Ctx, fun(Name) -> wallaroo:get_branch(Name) end, branch).
 
 content_types_accepted(ReqData, Ctx) ->
     {[{"application/json", from_json}], ReqData, Ctx}.
@@ -28,9 +28,9 @@ finish_request(ReqData, Ctx) ->
     {true, ReqData, Ctx}.
 
 to_json(ReqData, Ctx) ->
-    wallaroo_web_common:generic_to_json(ReqData, Ctx, fun(_) -> wallaroo:list_tags() end, fun(Name, _) -> wallaroo:get_tag(Name) end, false).
+    wallaroo_web_common:generic_to_json(ReqData, Ctx, fun(_) -> wallaroo:list_branches() end, fun(Name, _) -> wallaroo:get_branch(Name) end, false).
 			      
 from_json(ReqData, Ctx) ->
-    wallaroo_web_common:generic_from_json(ReqData, Ctx, fun(_) -> ok end, tag, "tags").
+    wallaroo_web_common:generic_from_json(ReqData, Ctx, fun(_) -> ok end, branch, "branches").
 
 								
