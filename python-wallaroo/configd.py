bd2758d19257ab45e023fb3031a83302ee157def
William Benton <willb@redhat.com>
2013-01-29 22:52:01 -0600

    configd.py updated for wallaroo

--- python-wallaroo/configd.py
+++ python-wallaroo/configd.py
@@ -1,43 +1,49 @@
 #!/usr/bin/python
 
+import os
 import shlex
 import socket
 import subprocess
 import time
-import wallaby
-from qmf.console import Session
+import wallaroo.client
 
-session = Session ()
-broker = session.addBroker()
+store = wallaroo.client.connect(**{"tag":"current"})
 
-obj, = session.getObjects(_class="Store", _package='com.redhat.grid.config')
-store = wallaby.Store(obj, session)
+conf_file = "condor_config.wallaroo"
+ver = None
+curr_ver = 0
 
-cmds = shlex.split("condor_config_val WALLABY_CONFIG_VERSION")
-cobj = subprocess.Popen(cmds, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-(out, err) = cobj.communicate()
-curr_ver = out.strip()
-print "My current version is '%s'" % str(curr_ver)
+proper = os.getenv("WALLAROO_PROPER") is not None
 
-cmds = shlex.split("condor_config_val LOCAL_CONFIG_FILE")
-cobj = subprocess.Popen(cmds, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-(out, err) = cobj.communicate()
-conf_file = out.strip()
-print "Writing config to '%s'" % conf_file
+if proper:
+    cmds = shlex.split("condor_config_val WALLABY_CONFIG_VERSION")
+    cobj = subprocess.Popen(cmds, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    (out, err) = cobj.communicate()
+    curr_ver = out.strip()
+    print "My current version is '%s'" % str(curr_ver)
+    
+    cmds = shlex.split("condor_config_val LOCAL_CONFIG_FILE")
+    cobj = subprocess.Popen(cmds, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    (out, err) = cobj.communicate()
+    conf_file = out.strip()
+    print "Writing config to '%s'" % conf_file
 
 name = socket.gethostname()
 while True:
   print "Checking version in store"
   node = store.getNode(name)
-  ver = str(node.last_updated_version)
   if ver != curr_ver:
     print "Writing config version '%s'" % str(ver)
-    config = node.getConfig({'version':ver})
+    config = node.getConfig()
     fobj = open(conf_file, 'w')
     fobj.seek(0, 0)
     for key in config.keys():
       fobj.write("%s = %s\n" % (key, config[key]))
     fobj.close()
     curr_ver = ver
-  time.sleep(10)
+    if proper:
+        cobj = subprocess.Popen(["condor_reconfig"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        (out, err) = cobj.communicate()
+        print out
+  time.sleep(45)
 
