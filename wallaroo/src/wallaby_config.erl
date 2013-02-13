0d30c9764be108a6db81568589e3e9d04b016337
William Benton <willb@redhat.com>
2013-02-12 19:57:06 -0600

    added wallaby_config:reset/0

--- wallaroo/src/wallaby_config.erl
+++ wallaroo/src/wallaby_config.erl
@@ -8,13 +8,13 @@
 
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
 
--export([has/3, for/3, cache_dump/0]).
-
--compile([export_all]).
+-export([has/3, for/3, cache_dump/0, reset/0]).
 
 -record(cstate, {re, table, storage}).
 
 -define(SERVER, ?MODULE).
+
+-include("dlog.hrl").
 
 start_link() ->
     Result = gen_server:start_link({local, ?SERVER}, ?MODULE, [], []),
@@ -45,12 +45,17 @@ for(Kind, Name, Commit) ->
 cache_dump() ->
     gen_server:call(?SERVER, {cache_dump}).
 
+reset() ->
+    gen_server:call(?SERVER, {reset}).
+
 %%% gen_server callbacks
 handle_cast(stop, State) ->
     {stop, normal, State}.
 
 handle_call({cache_dump}, _From, #cstate{table=C}=State) ->
     {reply, ets:tab2list(C), State};
+handle_call({reset}, _From, #cstate{table=C}=State) ->
+    {reply, ets:delete_all_objects(C), State};
 handle_call({has_config, Kind, Name, Commit}, _From, #cstate{storage=StoreMod}=State) ->
     Result = generic_lookup(Kind, Name, Commit, State, false),
     case Result of
@@ -150,7 +155,7 @@ calc_one_config({Kind, Name}, Tree, Commit, #cstate{re=RE, table=Cache, storage=
     cache_store(Kind, Name, Commit, apply_to(BaseConfig, MyConfig, true, State), State);
 calc_one_config({Kind, Name}, Tree, Commit, #cstate{re=RE, table=Cache, storage=StoreMod}=State) 
   when Kind =:= 'group' ->
-    error_logger:warning_msg("wallaby_config:calc_one_config/4:  {~p,~p}, ~p, ~p~n", [Kind, Name, Tree, Commit]),
+    ?D_LOG("wallaby_config:calc_one_config/4:  {~p,~p}, ~p, ~p~n", [Kind, Name, Tree, Commit]),
     %% get group object from tree
     {value, EntityObj} = wallaroo_tree:get_path([path_for_kind(Kind), Name], Tree, StoreMod),
     %% apply installed feature configs to empty config; these should already be in the cache
@@ -241,7 +246,7 @@ cache_store(Kind, Name, Commit, Config, #cstate{table=Cache}) ->
     Config.
 
 cache_find(Kind, Name, Commit, #cstate{table=Cache}) ->
-    error_logger:warning_msg("cache_find/4 Kind=~p, Name=~p, Commit=~p~n", [Kind, Name, Commit]),
+    ?D_LOG("cache_find/4 Kind=~p, Name=~p, Commit=~p~n", [Kind, Name, Commit]),
     case ets:match(Cache, {{Kind, Name, Commit}, '$1'}) of
 	[[Config]] ->
 	    {value, Config};
