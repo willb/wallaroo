f5295291d7a56f3e325a5363c468307bd60a410f
William Benton <willb@redhat.com>
2013-04-05 11:25:38 -0500

    Add robust basic auth for tags

--- apps/wallaroo_web/src/wallaroo_tag_resource.erl
+++ apps/wallaroo_web/src/wallaroo_tag_resource.erl
@@ -5,12 +5,27 @@
 -module(wallaroo_tag_resource).
 -export([init/1, to_json/2, resource_exists/2]). 
 -export([from_json/2]).
--export([allowed_methods/2, content_types_provided/2, content_types_accepted/2, finish_request/2, delete_resource/2, delete_completed/2]).
+-export([is_authorized/2, allowed_methods/2, content_types_provided/2, content_types_accepted/2, finish_request/2, delete_resource/2, delete_completed/2]).
 
 -include_lib("webmachine/include/webmachine.hrl").
+-include_lib("wallaroo_web_auth.hrl").
 
 init(Args) ->
     wallaroo_web_common:generic_init(Args).
+
+is_authorized(ReqData, Ctx) ->
+    Method = wrq:method(ReqData),
+    Name = wrq:path_info(name, ReqData),
+    authorized_for(Method, Name, ReqData, Ctx).
+
+authorized_for(Method, "empty", ReqData, Ctx) when Method =:= 'PUT' orelse Method =:= 'POST' orelse Method =:= 'DELETE' ->
+    {{halt, 405}, ReqData, Ctx};
+authorized_for('DELETE', "current", ReqData, Ctx) ->
+    {{halt, 405}, ReqData, Ctx};
+authorized_for(Method, "current", ReqData, Ctx) when Method =:= 'PUT' orelse Method =:= 'POST' ->
+    wallaroo_web_common:generic_auth(ReqData, Ctx, admin);
+authorized_for(_Method, _Name, ReqData, Ctx) ->
+    ?STANDARD_AUTH(ReqData, Ctx).
 
 allowed_methods(ReqData, Ctx) ->
     {['HEAD', 'GET', 'POST', 'PUT', 'DELETE'], ReqData, Ctx}.
