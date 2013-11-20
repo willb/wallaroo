211530623499c9beb3354e18b3931886a8e4ad74
William Benton <willb@redhat.com>
2013-11-20 12:55:43 -0600

    version.rb now works with Ruby 1.9 and above

--- version.rb
+++ version.rb
@@ -5,7 +5,7 @@ module Wallaroo
      def self.init
        # XXX:  do I even need to mention that this method 
        # should be refactored in the very near future?
-       items = Hash[*open("./apps/wallaroo/src/version.hrl", "r") {|f| f.read}.grep(/.*?(major|minor|patch|build),\s*(.*?)\}.*/).map {|s| s.gsub(/.*?(major|minor|patch|build),\s*(.*?)\}.*/, "\\1=\\2").chomp}.map {|s| res = s.split("="); [res[0].upcase.to_sym, res[1]]}.flatten]  
+       items = Hash[*open("./apps/wallaroo/src/version.hrl", "r") {|f| f.read}.split("\n").grep(/.*?(major|minor|patch|build),\s*(.*?)\}.*/).map {|s| s.gsub(/.*?(major|minor|patch|build),\s*(.*?)\}.*/, "\\1=\\2").chomp}.map {|s| res = s.split("="); [res[0].upcase.to_sym, res[1]]}.flatten]  
        [:MAJOR, :MINOR, :PATCH].each {|k| const_set(k, items[k].to_i) }
        build = items[:BUILD].gsub(/\A"|"\Z/, '')
        build = nil unless build.size > 0
